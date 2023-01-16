eddb51cb922f03c96c3305e137ae32a2613597b8
bobkovIBAS <va.bobkov@yandex.ru>
2023-01-16 04:01:29 +0400

    Создана регистрация и авторизация

---
+++ src/main/java/com/RosGramm/server/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.RosGramm.server.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
---
+++ src/main/java/com/RosGramm/server/user/Role.java
@@ -0,0 +1,6 @@
+package com.RosGramm.server.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
